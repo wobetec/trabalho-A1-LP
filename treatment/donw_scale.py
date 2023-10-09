c716092aaaf5e9b1047f4026139cf63c940f8201
wobetec <esdras2821@gmail.com>
2023-10-09 12:41:46 -0300

    first commit, data, folder structure

---
+++ treatment/donw_scale.py
@@ -0,0 +1,34 @@
+columns = [
+    "TP_FAIXA_ETARIA", # faixa etária
+    "TP_SEXO", # sexo
+    "TP_ESTADO_CIVIL", # estado civil
+    "TP_COR_RACA", # cor/raça
+    "TP_ST_CONCLUSAO", # situação de conclusão do ensino médio
+    "TP_ANO_CONCLUIU", # ano de conclusão do ensino médio
+    "TP_ESCOLA", # tipo de escola do ensino médio
+    "IN_TREINEIRO", # é treineiro?
+    "SG_UF_PROVA", # estado de prova
+    "TP_PRESENCA_CN", # presença na prova de ciências da natureza
+    "TP_PRESENCA_CH", # presença na prova de ciências humanas
+    "TP_PRESENCA_LC", # presença na prova de linguagens e códigos
+    "TP_PRESENCA_MT", # presença na prova de matemática
+    "NU_NOTA_CN", # nota da prova de ciências da natureza
+    "NU_NOTA_CH", # nota da prova de ciências humanas
+    "NU_NOTA_LC", # nota da prova de linguagens e códigos
+    "NU_NOTA_MT", # nota da prova de matemática
+    "TP_LINGUA", # língua estrangeira
+    "TP_STATUS_REDACAO", # status da redação
+    "NU_NOTA_COMP1", # nota da competência 1 da redação
+    "NU_NOTA_COMP2", # nota da competência 2 da redação
+    "NU_NOTA_COMP3", # nota da competência 3 da redação
+    "NU_NOTA_COMP4", # nota da competência 4 da redação
+    "NU_NOTA_COMP5", # nota da competência 5 da redação
+    "NU_NOTA_REDACAO", # nota da redação
+    "Q005", # quantidade de pessoas que moram na residência
+    "Q006", # renda mensal familiar
+    "Q019", # televisão a cores?
+    "Q021", # tv por assinatura?
+    "Q022", # telefone celular?
+    "Q024", # computador?
+    "Q025", # internet?
+]
