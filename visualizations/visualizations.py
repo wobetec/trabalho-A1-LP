3dab76acacfce40d96eafc7effa69a1d8a6d243a
wobetec <esdras2821@gmail.com>
2023-10-14 22:11:35 -0300

    correções de formatação

--- visualizations/visualizations.py
+++ visualizations/visualizations.py
@@ -3,6 +3,7 @@ import pandas as pd
 import base64
 from io import BytesIO
 
+
 def get_image_to_flask() -> str:
     """
     Transforma a figura do matplotlib em uma sequência de bytes para ser usada no Flask e retorna a tag imagem já com esses bytes
@@ -11,12 +12,13 @@ def get_image_to_flask() -> str:
 
     Returns:
         image (str): Tag imagem com os bytes da figura
-    
+
     """
     buf = BytesIO()
-    plt.savefig(buf, format='png')
+    plt.savefig(buf, format="png")
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return f"<img src='data:image/png;base64,{data}'/>"
+
 
 def esdras(df: pd.DataFrame) -> tuple:
     """
@@ -29,16 +31,28 @@ def esdras(df: pd.DataFrame) -> tuple:
         image (str): Tag imagem com os bytes da figura
         text (list): Lista com os textos para serem exibidos na página
         title (str): Título da visualização
-    
-    """
-    df["media"] = (df["NU_NOTA_CH"] + df["NU_NOTA_CN"] + df["NU_NOTA_LC"] + df["NU_NOTA_MT"] + df["NU_NOTA_REDACAO"])/5
 
-    amostra = df[["media", "Q006"]].groupby("Q006").mean().sort_values(by="media", ascending=True).reset_index()
+    """
+    df["media"] = (
+        df["NU_NOTA_CH"]
+        + df["NU_NOTA_CN"]
+        + df["NU_NOTA_LC"]
+        + df["NU_NOTA_MT"]
+        + df["NU_NOTA_REDACAO"]
+    ) / 5
+
+    amostra = (
+        df[["media", "Q006"]]
+        .groupby("Q006")
+        .mean()
+        .sort_values(by="media", ascending=True)
+        .reset_index()
+    )
 
     plt.bar(amostra["Q006"], amostra["media"])
-    plt.xlabel('Renda Familiar')
-    plt.ylabel('Média das Notas')
-    plt.title('Comparação de Renda e Notas')
+    plt.xlabel("Renda Familiar")
+    plt.ylabel("Média das Notas")
+    plt.title("Comparação de Renda e Notas")
 
     text = [
         "Primeiramente, vale ressaltar que A é nenhuma renda familiar e Q é mais de 20 salários mínimos, que no ano em questão valia R$ 1212,00.",
@@ -49,6 +63,7 @@ def esdras(df: pd.DataFrame) -> tuple:
     title = "Renda e Nota - Esdras"
 
     return get_image_to_flask(), text, title
+
 
 def angelo(df: pd.DataFrame) -> tuple:
     """
@@ -61,71 +76,69 @@ def angelo(df: pd.DataFrame) -> tuple:
             image (str): Tag imagem com os bytes da figura
             text (list): Lista com os textos para serem exibidos na página
             title (str): Título da visualização
-    
+
     """
-    cross_tab = pd.crosstab(df['Q006'], df['TP_FAIXA_ETARIA'])
+    cross_tab = pd.crosstab(df["Q006"], df["TP_FAIXA_ETARIA"])
 
-    ax = cross_tab.plot(kind='bar', stacked=True, colormap='viridis')
+    ax = cross_tab.plot(kind="bar", stacked=True, colormap="viridis")
 
-    plt.xlabel('Renda Familiar')
-    plt.ylabel('Contagem')
-    plt.title('Distribuição da Faixa Etária pela Renda Familiar - Angelo')
-    ax.legend(title='Faixa Etária', loc='center left', bbox_to_anchor=(1.0, 0.5))
-    plt.ticklabel_format(style='plain', axis='y')
-
+    plt.xlabel("Renda Familiar")
+    plt.ylabel("Contagem")
+    plt.title("Distribuição da Faixa Etária pela Renda Familiar - Angelo")
+    ax.legend(title="Faixa Etária", loc="center left", bbox_to_anchor=(1.0, 0.5))
+    plt.ticklabel_format(style="plain", axis="y")
 
     text = [
         "Observamos que, em algumas situações, pessoas com recursos financeiros limitados optam por realizar a prova após a idade média esperada.",
         "A desigualdade social desempenha um papel crucial no acesso à educação de qualidade, e essa visualização ressalta a complexidade das decisões educacionais em um contexto de desigualdade.",
         "A análise destaca a necessidade contínua de promover a equidade educacional e combater a desigualdade no acesso à educação."
-        "A crítica sugere que a falta de oportunidade de estudos por conta da renda familiar acarreta no tardio ingresso no mercado de trabalho."
+        "A crítica sugere que a falta de oportunidade de estudos por conta da renda familiar acarreta no tardio ingresso no mercado de trabalho.",
     ]
 
     title = "Oportunidade de Estudo - Angelo"
 
     return get_image_to_flask(), text, title
 
-def lara(df):
-"""
-Visualização que mostra a porcentagem de participantes do gênero feminino e masculino, separando pelas raças também declaradas por esses participantes.
-    Parametros:
-            df (pd.DataFrame): DataFrame com os dados
 
-        Returns:
-            image (str): Tag imagem com os bytes da figura
-            text (list): Lista com os textos para serem exibidos na página
-            title (str): Título da visualização
-"""
+def lara(df: pd.DataFrame) -> tuple:
+    """
+    Visualização que mostra a porcentagem de participantes do gênero feminino e masculino, separando pelas raças também declaradas por esses participantes.
+
+    Parametros:
+        df (pd.DataFrame): DataFrame com os dados
+
+    Returns:
+        image (str): Tag imagem com os bytes da figura
+        text (list): Lista com os textos para serem exibidos na página
+        title (str): Título da visualização
+    """
+
     # Agrupa os dados por gênero e raça, também conta a quantidade em cada grupo
-    grouped_data = df.groupby(['TP_COR_RACA', 'TP_SEXO']).size().unstack()
+    grouped_data = df.groupby(["TP_COR_RACA", "TP_SEXO"]).size().unstack()
 
     # Cores para as barras (F e M)
-    cores = ['pink', 'blue']
+    cores = ["pink", "blue"]
 
     # Cria o gráfico de barras agrupadas com as cores que escolhi
-    grouped_data.plot(kind='bar', stacked=True, color=cores)
+    grouped_data.plot(kind="bar", stacked=True, color=cores)
 
     # Adiciona rótulos aos eixos e mantém os números do eixo x retos
-    plt.xlabel('Raça')
-    plt.ylabel('Contagem')
-    plt.xticks(rotation=0)  
+    plt.xlabel("Raça")
+    plt.ylabel("Contagem")
+    plt.xticks(rotation=0)
 
     # Adiciona título ao gráfico
-    plt.title('Distribuição de Gênero por Raça no ENEM')
+    plt.title("Distribuição de Gênero por Raça no ENEM")
     text = [
         "É possível notar que os grupos 1 e 3 são os maiores, sendo então candidatos brancos ou pardos maioria no ENEM (também com um grande ppublico feminino).",
         "Em terceiro lugar de tamanho, o grupo 2 mostra os participantes negros (com grande quantidade de mulheres), sendo uma das raças mais presentes no Brasil segundo o IBGE.",
         "O abismo de quantidade do grupo 2 para o 1 e 3 é considerável, não condizendo com a diferença populacional deles, mostrando uma possível marginalização do grupo."
-        "Cidadãos negros, mesmo que em grande quantidade, não estão tão presentes no concurso público quanto brancos ou pardos (indígenas também apareceram em número mínimo)."
+        "Cidadãos negros, mesmo que em grande quantidade, não estão tão presentes no concurso público quanto brancos ou pardos (indígenas também apareceram em número mínimo).",
     ]
 
-    # Exibe o gráfico
-    plt.show()
+    title = "Raça e Gênero - Lara"
 
-# Chame a função com o DataFrame df
-lara(df)
-
-return get_image_to_flask(), text, title
+    return get_image_to_flask(), text, title
 
 
 def get_all_vis(df: pd.DataFrame) -> list:
@@ -137,11 +150,12 @@ def get_all_vis(df: pd.DataFrame) -> list:
 
     Returns:
         vis (list): Lista com todas as visualizações
-    
+
     """
     vis = []
 
     vis.append(esdras(df))
     vis.append(angelo(df))
+    vis.append(lara(df))
 
     return vis
