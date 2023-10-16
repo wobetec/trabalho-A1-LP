7425850c2229231d89d354b59f9bf4afc0f941ce
wobetec <esdras2821@gmail.com>
2023-10-16 08:38:17 -0300

    read the docs with theme

--- sphinx-scripts/conf.py
+++ sphinx-scripts/conf.py
@@ -17,7 +17,7 @@ author = 'group'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -29,3 +29,4 @@ language = 'pt_BR'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
